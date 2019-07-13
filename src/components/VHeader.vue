41bb683cd4026dca8c4252ea8b060424197e384c
Yukhmanov Stepan <flowstepg@gmail.com>
2019-07-14 09:06:49 +1000

    Add header

---
+++ src/components/VHeader.vue
@@ -0,0 +1,110 @@
+<template>
+  <header class="header">
+    <nav class="navbar is-transparent" role="navigation" aria-label="main navigation">
+      <div class="container">
+
+        <div class="navbar-brand is-hidden-desktop">
+          <a class="navbar-item" href="https://www.dvfu.ru/" target="_blank">
+            <img class="header__logo header__logo--mobile" src="@/assets/fefu-logo.svg">
+          </a>
+
+          <a
+            role="button"
+            class="navbar-burger burger"
+            aria-label="menu"
+            aria-expanded="false"
+            data-target="navbarBasicExample"
+            @click="toggleMenu"
+            :class="{ 'is-active': isMenuOpen }"
+          >
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
+        </div>
+
+        <div
+          id="navbarBasicExample"
+          class="navbar-menu"
+          :class="{
+            'is-active': isMenuOpen
+          }"
+        >
+          <div class="navbar-start">
+            <a class="navbar-item">
+              Главная
+            </a>
+
+            <div class="navbar-item has-dropdown is-hoverable">
+              <a class="navbar-link">
+                Работы
+              </a>
+
+              <div class="navbar-dropdown is-boxed">
+                <a href="#" class="navbar-item">Лабораторная работа №1</a>
+                <a href="#" class="navbar-item">Лабораторная работа №2</a>
+                <a href="#" class="navbar-item">Лабораторная работа №3</a>
+                <a href="#" class="navbar-item">Лабораторная работа №4</a>
+                <a href="#" class="navbar-item">Лабораторная работа №5</a>
+                <a href="#" class="navbar-item">Лабораторная работа №6</a>
+                <a href="#" class="navbar-item">Лабораторная работа №7</a>
+                <hr class="navbar-divider">
+                <a class="navbar-item">
+                  Какой-то пункт
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div class="navbar-brand is-hidden-touch">
+            <a class="navbar-item" href="https://www.dvfu.ru/" target="_blank">
+              <img src="@/assets/fefu-logo.svg">
+            </a>
+          </div>
+
+          <div class="navbar-end">
+            <div class="navbar-item">
+              <div class="field is-grouped">
+                <p class="control">
+                  <a class="button" target="_blank" href="https://github.com/StepFlow/econometrics">
+                    <span class="icon">
+                      <i class="fab fa-github"></i>
+                    </span>
+                    <span>
+                      GitHub
+                    </span>
+                  </a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        isMenuOpen: false
+      }
+    },
+    methods: {
+      toggleMenu() {
+        this.isMenuOpen = !this.isMenuOpen
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  .header {
+    &__logo {
+      &--mobile {
+        height: 2.75rem;
+      }
+    }
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+
+import VHeader from '@/components/VHeader'
+
+import '@fortawesome/fontawesome-free/js/all.min'
 
 Vue.config.productionTip = false
 
+Vue.component('v-header', VHeader)
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
