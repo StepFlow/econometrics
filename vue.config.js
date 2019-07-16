69bde7e7e7a5fe0bf0e92d9d07b156abde54da3d
Yukhmanov Stepan <flowstepg@gmail.com>
2019-07-17 08:03:30 +1000

    fix

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/econometrics/'
-    : '/',
+  publicPath: 'econometrics',
   css: {
     loaderOptions: {
       // pass options to sass-loader
